8b6f88d08148a7720bf2d13b49bfdd44f7573556
reubenjohn <reubenjohn@users.noreply.github.com>
2021-03-14 13:56:43 +0000

    Deploying to gh-pages from @ MJohnJoy/manappallil@6482d485d95f135bb23d7d8b6ea6cc8b0e4cb0e3 🚀

--- precache-manifest.53b7c6887b70743cc08c3d5a647665f9.js
+++ precache-manifest.53b7c6887b70743cc08c3d5a647665f9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0759fea3c7b976d27e93278b029066d8",
+    "revision": "98be93cf21dde84e51f2560b6368e469",
     "url": "/manappallil/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/manappallil/static/css/2.5c17b9f1.chunk.css"
   },
   {
-    "revision": "8f3649a332dc92db0811",
-    "url": "/manappallil/static/css/main.609bce1a.chunk.css"
+    "revision": "176553e9f1a36bbf167a",
+    "url": "/manappallil/static/css/main.cdcf92e3.chunk.css"
   },
   {
     "revision": "5023434d8044fd453d17",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/manappallil/static/js/2.74334acb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8f3649a332dc92db0811",
-    "url": "/manappallil/static/js/main.0b04e617.chunk.js"
+    "revision": "176553e9f1a36bbf167a",
+    "url": "/manappallil/static/js/main.49161099.chunk.js"
   },
   {
     "revision": "740d791f37ae701e1000",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/manappallil/precache-manifest.3591dc95708f471f417f2753e4538425.js"
+  "/manappallil/precache-manifest.53b7c6887b70743cc08c3d5a647665f9.js"
 );
 
 self.addEventListener('message', (event) => {
